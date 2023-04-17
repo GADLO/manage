19608de51fadcab111027ebcaff4eb6dc4503b68
GADLO <geekarea@outlook.com>
2023-04-17 16:59:45 +0800

    ✨ feat:header编写

---
+++ manage/src/components/ManageIndex.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="hd-wrap">
+
+        <div class="hd">
+            <i class="el-icon-s-unfold" @click="handleClose"></i>
+            <img src="" alt="LOGO">
+            <span>管理系统</span>
+        </div>
+        <el-drawer title="我是标题" :visible.sync="drawer" direction="ltr">
+            <span @click='handleClose'>我来啦!</span>
+        </el-drawer>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            drawer: false
+
+        }
+    },
+    methods: {
+        handleClose(done) {
+            this.drawer = !this.drawer
+        }
+    }
+}
+</script>
+
+<style lang='scss'>
+.hd-wrap {
+    padding: 0;
+}
+
+.hd {
+    background-image: linear-gradient(315deg, #00b6ff, #006bff);
+    color: #fff;
+    font-size: large;
+    font-weight: bold;
+    padding: 10px;
+    text-align: left;
+
+    img {
+        margin: 10px;
+    }
+}
+</style>
--- manage/src/router/index.js
+++ manage/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import ManageIndex from "../components/ManageIndex";
+
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'ManageIndex',
+      component: ManageIndex
     }
   ]
 })
